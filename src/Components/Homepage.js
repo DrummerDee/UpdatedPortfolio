321cba0171e6bf64348d7031289bdf8450a18fd2
Drummer Dee <drummer.deja@gmail.com>
2023-07-29 22:44:24 -0400

    fixed viewport

--- src/Components/Homepage.js
+++ src/Components/Homepage.js
@@ -7,45 +7,39 @@ import Button from '@mui/material/Button';
 import '../Styles/Homepage.css';
 
 
-
 export default function Homepage() {
     return (
         <Container
-            
-            sx={{ mr:2 }}
-            style ={{backgroundColor:'whitesmoke', margin:'0',padding:'0'}}
+            maxWidth = 'xl'
+            style={{ backgroundColor: 'whitesmoke', margin: '0', padding: '0' }}
         >
             <div>
-                <section className = 'container__left'> 
-                <div className = "left_wrapper">
-                <Typography variant="h5"> Hello, my name is </Typography>
-                <Typography variant="h2"> Deja </Typography> 
-                <Typography variant="h2"> Drummer </Typography>
-                <h4>Your next </h4> 
-                <div className = 'descrip'>
-                    <div className = 'descrip__wrapper'>
-                        <div className = 'title'> UI/UX Designer </div>
-                        <div className = 'title'> Software Engineer </div>
-                        <div className = 'title'> Team Player </div>
-                        <div className = 'title'> Profound Employee </div>
+                <section className='container__left'>
+                    <div className="left_wrapper">
+                        <Typography variant="h5"> Hello, my name is </Typography>
+                        <Typography variant="h2"> Deja </Typography>
+                        <Typography variant="h2"> Drummer </Typography>
+                        <Typography variant="h4">Your next </Typography>
+                        <div className='descrip'>
+                            <div className='descrip__wrapper'>
+                                <div className='title'> UI/UX Designer </div>
+                                <div className='title'> Software Engineer </div>
+                                <div className='title'> Team Player </div>
+                                <div className='title'> Profound Employee </div>
+                            </div>
+                        </div>
+                        <div>
+                            <Button
+                                variant='outlined'
+                                size='medium'
+                                href='#'
+                                style={{backgroundColor:'#702963', color:'white', margin:'1.5rem 2rem'}}
+                            > Meet Me </Button>
                         </div>
                     </div>
-            <div>
-                    <Button 
-                        variant='outlined' 
-                        size = 'medium'
-                        href='#' 
-                        className = 'left__btn'
-                        > Meet Me </Button>
-                        </div>
-                </div>
                 </section>
             </div>
-            <Box
-            sx={{width:'400',height:'500'}}
-            >
-                <span> This shouldn't in mobile</span>
-                </Box>
+
         </Container>
     )
 }
